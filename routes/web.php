e48b73b2a96e123615d3fb5d51e0c372f494f139
Simeia Olimpia <soss@discente.ifpe.edu.br>
2025-11-02 09:44:02 -0300

    Atividade 05 - Part 1 - Autenticação

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+//use App\Http\Controllers\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('products', ProductController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
